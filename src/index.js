84155f779554cbe7c47d705a600ac62a008aa024
Donald Akite <donaldakite27@gmail.com>
2022-12-30 19:20:24 +0000

    Add login

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './style.css';
+// eslint-disable-next-line import/no-named-as-default-member, import/no-named-as-default
+import signup from './modules/signup.js';
+// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
+import loginPage from './modules/login.js';
 
 const slideimg1 = '<img src="img/13.png" alt="popUp-Item">';
 const slideimg2 = '<img src="img/ipone13.png" alt="popUp-Item">';
@@ -7,8 +11,17 @@ const sec = document.querySelector('.first-sec');
 const imageContent1 = document.querySelector('.slide1');
 const imageContent2 = document.querySelector('.slide2');
 const imageContent3 = document.querySelector('.slide3');
-const imgBox = document.querySelector('.imgBox');
+const imgBox = document.querySelector('.img-box');
+const Account = document.querySelector('.item_user');
+const UI = document.querySelector('.content');
 
+Account.addEventListener('click', () => {
+  UI.innerHTML = loginPage;
+  const signupbtn = document.querySelector('.login-title p');
+  signupbtn.addEventListener('click', () => {
+    UI.innerHTML = signup;
+  });
+});
 imageContent1.addEventListener('click', () => {
   imgBox.innerHTML = slideimg1;
   sec.style.background = '#5a7894';
@@ -21,3 +34,18 @@ imageContent3.addEventListener('click', () => {
   imgBox.innerHTML = slideimg3;
   sec.style.background = '#0062be';
 });
+
+// let state = false;
+// const eye = document.querySelector('.eye');
+// const eyeid = document.querySelector('#eye');
+// eye.addEventListener('click', () => {
+//   if (state) {
+//     document.getElementById('password').setAttribute('type', 'password');
+//     state = false;
+//     eyeid.style.color = '#7a797e';
+//   } else {
+//     document.getElementById('password').setAttribute('type', 'text');
+//     state = true;
+//     eyeid.style.color = '#5887ef';
+//   }
+// });
