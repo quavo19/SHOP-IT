e8436a0493c8e300cb30b7ccbbfed86c926008d0
Donald Akite <donaldakite27@gmail.com>
2022-12-30 19:23:02 +0000

    Add signup form

---
+++ src/modules/signup.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
+const signup = `<section class="section-2">
+<div class="content-2">
+    <div class="login-title">
+       <h3>Signup</h3>
+       
+    </div>
+    <div class="form">
+        <input type="text" id="email" class="form__input" autocomplete="off" placeholder=" ">
+        <label for="email" class="form__label">Email</label>
+    </div>
+    <div class="form2">
+        <input type="password" id="password" class="form2__input2" autocomplete="off" placeholder=" ">
+        <label for="password" class="form2__label">Password</label>
+        <span class="eye"><i class="fa fa-eye" id ="eye" aria-hidden="true"></i></span>
+    </div>
+    <div class="form2">
+        <input type="password" id="re-password" class="form2__input2" autocomplete="off" placeholder=" ">
+        <label for="re-password" class="form2__label">Re-Password</label>
+        <span class="eye"><i class="fa fa-eye" id ="eye" aria-hidden="true"></i></span>
+    </div>
+    <div class="form2">
+        <input type="text" id="name" class="form2__input2" autocomplete="off" placeholder=" ">
+        <label for="name" class="form2__label">User Name</label>
+    </div>
+    <div class="login-btn">
+        <button>Sign-up</button>
+    </div>
+</div>
+</section>`;
+
+export default signup;
