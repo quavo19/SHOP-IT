0a8befbee498a99c9d0d070dddd49bfa751d0e60
Donald Akite <donaldakite27@gmail.com>
2022-12-30 12:20:40 +0000

    Add color changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 
 const slideimg1 = `<img src="img/13.png" alt="popUp-Item">`
 const slideimg2 = `<img src="img/ipone13.png" alt="popUp-Item">`
-const slideimg3 = `<img src="img/ipone12.png" alt="popUp-Item">`
+const slideimg3 = `<img src="img/13pm.png" class="slide3" alt="popUp-Item">`
 const sec = document.querySelector('.first-sec')
 const imageContent1 = document.querySelector('.slide1')
 const imageContent2 = document.querySelector('.slide2')
@@ -19,5 +19,5 @@ imageContent2.addEventListener('click', () => {
 });
 imageContent3.addEventListener('click', () => {
     imgBox.innerHTML = slideimg3;
-    sec.style.background = '#22272c';
+    sec.style.background = '#0062be';
 });
