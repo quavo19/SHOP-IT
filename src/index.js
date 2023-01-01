ff15de99eb6e49f8f056bd102d9f55649d1b5148
Donald Akite <donaldakite27@gmail.com>
2023-01-01 18:02:36 +0000

    import modules

--- src/index.js
+++ src/index.js
@@ -7,18 +7,11 @@ import loginPage from './modules/login.js';
 import Eye from './modules/pasword.js';
 // eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
 import homePage from './modules/home.js';
+// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
+import smallSlide from './modules/small-slider.js';
+// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
 import cartBox from './modules/cart.js';
 
-homePage();
-
-const slideimg1 = '<img src="img/13.png" alt="popUp-Item">';
-const slideimg2 = '<img src="img/ipone13.png" alt="popUp-Item">';
-const slideimg3 = '<img src="img/13pm.png" class="slide3" alt="popUp-Item">';
-const sec = document.querySelector('.first-sec');
-const imageContent1 = document.querySelector('.slide1');
-const imageContent2 = document.querySelector('.slide2');
-const imageContent3 = document.querySelector('.slide3');
-const imgBox = document.querySelector('.img-box');
 const Account = document.querySelector('.item_user');
 const UI = document.querySelector('.content');
 
@@ -31,17 +24,7 @@ Account.addEventListener('click', () => {
     Eye();
   });
 });
-imageContent1.addEventListener('click', () => {
-  imgBox.innerHTML = slideimg1;
-  sec.style.background = '#5a7894';
-});
-imageContent2.addEventListener('click', () => {
-  imgBox.innerHTML = slideimg2;
-  sec.style.background = '#1c3e5e';
-});
-imageContent3.addEventListener('click', () => {
-  imgBox.innerHTML = slideimg3;
-  sec.style.background = '#0062be';
-});
 
+smallSlide();
+homePage();
 cartBox();
