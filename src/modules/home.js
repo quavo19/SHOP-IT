33c32f57b5a30584f1176c0af9a4b7d5750ddd06
Donald Akite <donaldakite27@gmail.com>
2023-01-01 11:38:13 +0000

    Add function to take a user back to home

---
+++ src/modules/home.js
@@ -0,0 +1,24 @@
+const homePage = () => {
+  const home = `
+<div class="text-box">
+                <h2>
+                    That's What <br> <Span>i like</Span>
+                </h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                     Ratione id non ut, quod eaque sed amet placeat inventore odit eius
+                     hic alias saepe omnis dignissimos harum nobis laudantium debitis libero.
+                </p>
+                <a href="#">View All</a>
+            </div>
+            <div class="img-box">
+                <img src="img/13pm.png" class="pepsi" alt="coverimage">
+            </div> `;
+  const homeUI = document.querySelector('.content');
+  const homeContent = document.querySelector('.Home');
+
+  homeContent.addEventListener('click', () => {
+    homeUI.innerHTML = home;
+  });
+};
+
+export default homePage;
